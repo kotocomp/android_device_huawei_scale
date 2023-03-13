46ee5a0b28cce6d2a2c140d94f8b065a1f488686
kotocomp <111016680+kotocomp@users.noreply.github.com>
2023-03-14 00:44:21 +0600

    Update init_scale.c
    Update init_scale.c
    
    Update init_scale.c

--- common/init/init_scale.c
+++ common/init/init_scale.c
@@ -102,20 +102,14 @@ void init_msm_properties(unsigned long msm_id, unsigned long msm_ver, char *boar
         property_set("ro.build.fingerprint", "Huawei/SCL-L21/hwSCL-Q:5.1.1/HuaweiSCL-L21/C432B150:user/release-keys");
     }
 
-	/* U31 version a */
-    if (strstr(model, "MSM8909_SCL_U31_VA") != NULL) {
+	/* Y538 version b */
+    if (strstr(model, "MSM8909_Y538_A1_VB") != NULL) {
 	property_set("ro.product.model", "msm8909");
-        property_set("ro.product.name", "y6");
-        property_set("ro.product.device", "y6");
-        property_set("ro.build.product", "y6");
-	property_set("ro.config.dsds_mode", "umts_gsm");
-        property_set("persist.dsds.enabled", "true");
-        property_set("persist.radio.multisim.config", "dsds");
-	property_set("ro.multi.rid", "true");
-        property_set("ro.config.hw_showSimName", "true");
-        property_set("ro.dual.sim.phone", "true");
-        property_set("ro.build.description", "SCL-U31-user 5.1.1 GRJ90 C432B150 release-keys");
-        property_set("ro.build.fingerprint", "Huawei/SCL-U31/hwSCL-Q:5.1.1/HuaweiSCL-U31/C432B150:user/release-keys");
+        property_set("ro.product.name", "Y538");
+        property_set("ro.product.device", "hwY538");
+        property_set("ro.build.product", "Y538");
+        property_set("ro.build.description", "Y538-user 5.1.1 GRJ90 C237B150 release-keys");
+        property_set("ro.build.fingerprint", "Huawei/Y538/hwY538:5.1.1/HuaweiY538/C237B150:user/release-keys");
     }
 
 }
